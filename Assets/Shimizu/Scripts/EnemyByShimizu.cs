559b7cb0347cca0b4b22d0b3084499e4ac923603
prog470 <tshimizu470@stu.kanazawa-u.ac.jp>
2016-11-07 23:42:02 +0900

    ステージのサンプル

---
+++ Assets/Shimizu/Scripts/EnemyByShimizu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyByShimizu : MoveObjectBase{
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+        SetMoveToPlayer();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        base.OnTriggerEnter2D(other);
+        if (other.tag == "RightCorner" || other.tag == "LeftCorner")
+        {
+            this.GetComponent<Transform>().position = other.GetComponent<Transform>().position;
+        }
+        if (other.tag == "PlayerBullet")
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Shimizu/Scripts/PlayerBulleByShimizu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBulleByShimizu : MoveObjectBase
+{
+
+    protected override void Initialize(){
+        base.Initialize();
+        SetMoveToEnemy();
+    }
+
+    protected override void Update(){
+        base.Update();
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D other){
+        base.OnTriggerEnter2D(other);
+        if (other.tag == "RightCorner" || other.tag == "LeftCorner") {
+            this.GetComponent<Transform>().position = other.GetComponent<Transform>().position;
+        }
+        if (other.tag == "Enemy")
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+}
+ 
