6a67954e5265e0bc61b09d882e95c52f9e0631e3
yulon <yarboroughace@yahoo.co.jp>
2017-01-12 13:31:42 +0900

    Enemyプレハブとイメージ、ステージの追加

--- Assets/Application/Scripts/System/ENEMY_DEFINE.cs
+++ Assets/Application/Scripts/System/ENEMY_DEFINE.cs
@@ -4,15 +4,15 @@ using System;
 public static class ENEMY_DEFINE {
 
     public const int enemyVarietyNum = 10;
-    public static readonly ReadOnlyCollection<int> HP = Array.AsReadOnly(new int[] { 1, 2, 2, 2, 2, 2, 2, 2, 2, 2 });
-    public static readonly ReadOnlyCollection<int> DAMAGE = Array.AsReadOnly(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
-    public static readonly ReadOnlyCollection<string> PATH = Array.AsReadOnly(new string[] { "Prefabs/Enemy/Enemy",
-                                                                                              "Prefabs/Enemy/Enemy",
-                                                                                              "Prefabs/Enemy/Enemy",
-                                                                                              "Prefabs/Enemy/Enemy",
-                                                                                              "Prefabs/Enemy/Enemy",
-                                                                                              "Prefabs/Enemy/Enemy",
-                                                                                              "Prefabs/Enemy/Enemy",
+    public static readonly ReadOnlyCollection<int> HP = Array.AsReadOnly(new int[]	   { 1, 1, 1,  1, 1, 1, 2, 2, 2, 2 });
+    public static readonly ReadOnlyCollection<int> DAMAGE = Array.AsReadOnly(new int[] { 1, 1, 1, -1, 1, 1, 2, 1, 1, 1 });
+    public static readonly ReadOnlyCollection<string> PATH = Array.AsReadOnly(new string[] {  "Prefabs/Enemy/Enemy",
+                                                                                              "Prefabs/Enemy/Enemy2",
+                                                                                              "Prefabs/Enemy/Enemy3",
+                                                                                              "Prefabs/Enemy/Enemy4",
+                                                                                              "Prefabs/Enemy/Enemy5",
+                                                                                              "Prefabs/Enemy/Enemy6",
+                                                                                              "Prefabs/Enemy/Enemy7",
                                                                                               "Prefabs/Enemy/Enemy",
                                                                                               "Prefabs/Enemy/Enemy",
                                                                                               "Prefabs/Enemy/Enemy"});
