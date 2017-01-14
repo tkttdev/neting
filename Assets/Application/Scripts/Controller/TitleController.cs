3ba2085e3a1e9d7afdf68687e25436a5c24c1b8d
tatsuhito <takamiya.dev@gmail.com>
2017-01-14 21:38:25 +0900

    title変更 itweenの追加

--- Assets/Application/Scripts/Controller/TitleController.cs
+++ Assets/Application/Scripts/Controller/TitleController.cs
@@ -1,11 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TitleController : MonoBehaviour {
+
+	[SerializeField] private SpriteRenderer background;
+	[SerializeField] private Text tapText;
+
+	private float backgroundBlinkTime = 0.4f;
 
 	void Update(){
 		if (Input.GetMouseButtonDown (0)) {
 			AppSceneManager.I.GoScene (GameSceneType.MENU_SCENE);
 		}
+		BackgroundEffect ();
+	}
+
+	private void BackgroundEffect(){
+		background.color = new Color (background.color.r, background.color.g, background.color.b, Mathf.PingPong (Time.time * backgroundBlinkTime, 0.7f) + 0.3f);
 	}
 }
