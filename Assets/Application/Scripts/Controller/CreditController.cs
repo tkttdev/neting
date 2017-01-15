60f65048c73a7170103d10d9bd24a94c05df454c
tatsuhito <takamiya.dev@gmail.com>
2017-01-15 18:00:21 +0900

    微調整

---
+++ Assets/Application/Scripts/Controller/CreditController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditController : MonoBehaviour {
+
+	public void TitleButton(){
+		AppSceneManager.I.GoScene (GameSceneType.TITLE_SCENE);
+	}
+}
