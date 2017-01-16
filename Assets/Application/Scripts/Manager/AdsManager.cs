18da2dbef45dab41aa004b7255a6ae08ce08e2b7
tatsuhito <takamiya.dev@gmail.com>
2017-01-17 02:55:32 +0900

    バグ抜けするため敵の速度変更

---
+++ Assets/Application/Scripts/Manager/AdsManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : SingletonBehaviour<AdsManager> {
+
+	private bool beAbleWatch = true;
+	private const string ADS_KEY = "ableWatchAdsKey";
+
+	protected override void Initialize (){
+		base.Initialize ();
+		GetAdsInfo ();
+	}
+
+	public void ShowRewardedAd(){
+		if (Advertisement.IsReady ("rewardedVideo")) {
+			var options = new ShowOptions { resultCallback = HandleShowResult };
+			Advertisement.Show ("rewardedVideo", options);
+		}
+	}
+
+	private void HandleShowResult(ShowResult result){
+		switch (result){
+		case ShowResult.Finished:
+			UserDataManager.I.AddMoney (1000);
+			beAbleWatch = false;
+			SaveAdsInfo ();
+			break;
+		case ShowResult.Skipped:
+			break;
+		case ShowResult.Failed:
+			break;
+		}
+	}
+
+	public void EnableWatchAds(){
+		beAbleWatch = true;
+		SaveAdsInfo ();
+	}
+
+	private void SaveAdsInfo(){
+		PlayerPrefs.SetInt (ADS_KEY, beAbleWatch ? 1 : 0);
+	}
+
+	public bool GetAdsInfo(){
+		return (beAbleWatch = (PlayerPrefs.GetInt (ADS_KEY, 1) == 1));
+	}
+}
