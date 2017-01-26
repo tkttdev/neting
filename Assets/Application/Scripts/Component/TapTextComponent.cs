ca4d6ba692f266331e1fb2e318df8cb2cdf37dd9
tatsuhito <takamiya.dev@gmail.com>
2017-01-26 17:18:45 +0900

    commit

--- Assets/Application/Scripts/Component/TapTextComponent.cs
+++ Assets/Application/Scripts/Component/TapTextComponent.cs
@@ -2,14 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FingerComponent : MonoBehaviour {
+public class TapTextComponent : MonoBehaviour {
 
 	private float spendTime = 0.0f;
-
-	// Use this for initialization
-	void Start () {
-		iTween.MoveAdd (gameObject, iTween.Hash ("x", 0.3f, "y", 0.3f, "loopType", iTween.LoopType.pingPong,"easeType",iTween.EaseType.linear));
-	}
 	
 	// Update is called once per frame
 	void Update () {
