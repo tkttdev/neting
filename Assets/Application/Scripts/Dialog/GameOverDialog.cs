14639000a6416c49d083684b34015a831462bf79
tatsuhito <takamiya.dev@gmail.com>
2017-01-26 20:02:35 +0900

    animation追加

--- Assets/Application/Scripts/Dialog/GameOverDialog.cs
+++ Assets/Application/Scripts/Dialog/GameOverDialog.cs
@@ -9,6 +9,7 @@ public class GameOverDialog : DialogBase {
 	[SerializeField]private GameObject titleButton;
 	[SerializeField]private GameObject panel;
 	[SerializeField]private Text coinText;
+	[SerializeField]private Text gameOverText;
 
 	// Use this for initialization
 	protected override void Start() {
@@ -16,13 +17,9 @@ public class GameOverDialog : DialogBase {
 		Hide();
 	}
 
-	// Update is called once per frame
-	void Update() {
-		
-	}
-
 	public override void Show() {
 		base.Show();
+		StartCoroutine (ShowAnimation ());
 		coinText.text = GetItemManager.I.GetEarnMoney ().ToString ();
 		SetComponentsActive();
 	}
@@ -54,5 +51,22 @@ public class GameOverDialog : DialogBase {
 		UIManager.I.gameOverDialog.Hide ();
 		SoundManager.I.SoundSE (SE.BUTTON2);
 		AppSceneManager.I.GoScene(GameSceneType.MENU_SCENE);
+	}
+
+	private IEnumerator ShowAnimation(){
+		StartCoroutine (AlphaAnimation ());
+		yield return new WaitForSeconds (0.3f);
+		iTween.ScaleTo (panel, iTween.Hash ("y", 1.0f, "time", 0.3f));
+		yield break;
+	}
+
+	private IEnumerator AlphaAnimation(){
+		float a = 0.0f;
+		while (gameOverText.color.a < 1.0f) {
+			a += Mathf.Clamp (a + 0.05f, 0.0f, 1.0f);
+			gameOverText.color = new Color (gameOverText.color.r, gameOverText.color.g, gameOverText.color.b, a);
+			yield return new WaitForSeconds (0.1f);
+		}
+		yield break;
 	}
 }
--- Assets/Application/Scripts/Dialog/StageClearDialog.cs
+++ Assets/Application/Scripts/Dialog/StageClearDialog.cs
@@ -9,16 +9,30 @@ public class StageClearDialog : DialogBase {
 	[SerializeField]private GameObject titleButton;
 	[SerializeField]private GameObject panel;
 	[SerializeField]private Text coinText;
+	[SerializeField]private Text stageClearText;
+	[SerializeField]private Text rankText;
 
 	// Use this for initialization
 	protected override void Start() {
 		base.Start();
-		Hide();
+		Hide ();
 	}
 
 	public override void Show() {
 		base.Show();
+		StartCoroutine (ShowAnimation ());
 		coinText.text = GetItemManager.I.GetEarnMoney ().ToString ();
+		switch (GameCharacter.I.GetLife ()) {
+		case 3:
+			rankText.text = "RANK : S";
+			break;
+		case 2:
+			rankText.text = "RANK : A";
+			break;
+		case 1:
+			rankText.text = "RANK : B";
+			break;
+		}
 		SetComponentsActive();
 	}
 
@@ -50,5 +64,12 @@ public class StageClearDialog : DialogBase {
 		UIManager.I.stageClearDialog.Hide ();
 		SoundManager.I.SoundSE (SE.BUTTON2);
 		AppSceneManager.I.GoScene(GameSceneType.MENU_SCENE);
+	}
+
+	private IEnumerator ShowAnimation(){
+		iTween.ScaleTo (stageClearText.gameObject, iTween.Hash ("x", 0.8f, "y", 0.8f, "time", 0.5f));
+		yield return new WaitForSeconds (0.3f);
+		iTween.ScaleTo (panel, iTween.Hash ("y", 1.0f, "time", 0.3f));
+		yield break;
 	}
 }
