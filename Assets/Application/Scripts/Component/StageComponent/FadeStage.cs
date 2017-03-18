23d7307e2b9f61fa7d6734184fd5e3fefcd6c3b3
umecha1128 <umecha1128@gmail.com>
2017-03-18 17:48:21 +0900

    ステージ21,22,25追加

---
+++ Assets/Application/Scripts/Component/StageComponent/FadeStage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeStage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float a = 0;
+        iTween.ValueTo(gameObject,iTween.Hash("loopType",iTween.LoopType.pingPong,"easeType",iTween.EaseType.linear,"from",0.0f,"to",1.0f,"onupdate","SetAlpha"));
+	}
+	
+    private void SetAlpha(float _a){
+        gameObject.GetComponent<SpriteRenderer>().color =new Color(255,255,255,_a);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
