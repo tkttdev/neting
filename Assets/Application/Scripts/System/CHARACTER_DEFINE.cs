01c5bb2f4d3acab6e0e5bfc3d81c9bc4a73ae29a
yulon <yarboroughace@yahoo.co.jp>
2017-03-25 18:36:41 +0900

    キャラ調整

--- Assets/Application/Scripts/System/CHARACTER_DEFINE.cs
+++ Assets/Application/Scripts/System/CHARACTER_DEFINE.cs
@@ -15,7 +15,7 @@ public static class CHARACTER_DEFINE {
 	public static readonly ReadOnlyCollection<string> NAME                = Array.AsReadOnly(new string[] { "TYPE-ALPHA","TYPE-BETA","TYPE-GAMMA","TYPE-DELTA","TYPE-EPSILON","TYPE-ZETA","TYPE-ETA","TYPE-THETA","TYPE-IOTA","TYPE-KAPPA"});
 	public static readonly ReadOnlyCollection<int>    MONEY               = Array.AsReadOnly(new int[]    {    0,   2000,   4000,   6500,   9000,   11500,   14000,   18000,   25000,   50000 });
     public static readonly ReadOnlyCollection<int>    MAX_BULLET_STOCK    = Array.AsReadOnly(new int[]    {    3,    2,    5,    2,    1,    4,    3,    5,    4,    1 });
-    public static readonly ReadOnlyCollection<float>  BULLET_INTERVAL     = Array.AsReadOnly(new float[]  { 2.0f, 1.6f, 2.0f, 2.2f, 1.2f, 2.0f, 1.6f, 2.0f, 1.6f, 1.0f });
+    public static readonly ReadOnlyCollection<float>  BULLET_INTERVAL     = Array.AsReadOnly(new float[]  { 2.0f, 1.6f, 2.0f, 2.2f, 1.2f, 2.0f, 1.6f, 2.0f, 2.0f, 1.0f });
     public static readonly ReadOnlyCollection<int>    BULLET_DAMAGE       = Array.AsReadOnly(new int[]    {    1,    1,    1,    2,    1,    2,    1,    2,    3,    1 });
     public static readonly ReadOnlyCollection<string> BULLET_PREFAB_PATH  = Array.AsReadOnly(new string[] { "Prefabs/Bullet/CharacterBullet0",
                                                                                                             "Prefabs/Bullet/CharacterBullet1",
