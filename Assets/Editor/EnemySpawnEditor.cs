d44744bdd8b28603477b2abd08541bb62653c9a2
tatsuhito <takamiya.dev@gmail.com>
2017-06-01 00:44:45 +0900

    上下キーで移動できるように変更

--- Assets/Editor/EnemySpawnEditor.cs
+++ Assets/Editor/EnemySpawnEditor.cs
@@ -9,16 +9,20 @@ public class EnemySpawnEditor : EditorWindow {
 	private string[] testString = new string[] {"a", "b", "c", "d", "e", "f", "g", "h", "i"};
 	private static EnemyDefine enemyDefine;
 	private GameObject targetEnemy;
-	private int index = -1;
+	private int index = 0;
 	private Vector2 scroll;
 	private TextAsset stageCsv;
 	private Editor enemyEditor;
-
+	int selected;
 	[MenuItem("Window/EnemySpawnEdit")]
 	static void Open(){
 		enemyDefine = Resources.Load ("ScriptableObjects/EnemyDefineData") as EnemyDefine;
 		var window = GetWindow<EnemySpawnEditor> ();
 		window.maxSize = window.minSize = new Vector2 (700, 700);
+	}
+
+	void OnEnable(){
+		targetEnemy = Resources.Load (enemyDefine.enemy [index].PATH) as GameObject;
 	}
 
 	void OnGUI () {
@@ -39,7 +43,6 @@ public class EnemySpawnEditor : EditorWindow {
 
 		EditorGUILayout.BeginVertical ();
 		scroll = EditorGUILayout.BeginScrollView(scroll);
-
 		if (enemyDefine != null) {
 			for (int i = 0; i < enemyDefine.varietyNum; i++) {
 				bool flag = GUILayout.Toggle (index == i, "ENEMY" + i.ToString (), "OL Elem");
@@ -50,10 +53,21 @@ public class EnemySpawnEditor : EditorWindow {
 			}
 		}
 
+		if (curEvent.type == EventType.KeyDown && curEvent.keyCode == KeyCode.DownArrow) {
+			index = index < enemyDefine.varietyNum - 1 ? index + 1 : index;
+			targetEnemy = Resources.Load (enemyDefine.enemy [index].PATH) as GameObject;
+			Repaint ();
+		} else if (curEvent.type == EventType.KeyDown && curEvent.keyCode == KeyCode.UpArrow) {
+			index = index > 0 ? index - 1 : index;
+			targetEnemy = Resources.Load (enemyDefine.enemy [index].PATH) as GameObject;
+			Repaint ();
+		}
+
 		if (targetEnemy != null) {
 			Texture t = targetEnemy.GetComponent<SpriteRenderer> ().sprite.texture;
 			GUILayout.Box(t, GUILayout.Width(50), GUILayout.Height(50));
 		}
+
 
 		EditorGUILayout.EndScrollView();
 		EditorGUILayout.EndVertical ();
