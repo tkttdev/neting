431a66186987525738c007809f5ef276ac610958
tatsuhito <takamiya.dev@gmail.com>
2017-06-01 02:17:50 +0900

    targetEnemyを範囲内におけるように

--- Assets/Editor/EnemySpawnEditor.cs
+++ Assets/Editor/EnemySpawnEditor.cs
@@ -6,14 +6,22 @@ using System.IO;
 
 public class EnemySpawnEditor : EditorWindow {
 
-	private string[] testString = new string[] {"a", "b", "c", "d", "e", "f", "g", "h", "i"};
+	private Texture[] enemyTextures;
 	private static EnemyDefine enemyDefine;
 	private GameObject targetEnemy;
-	private int index = 0;
+	private int targetEnemyIndex = 0;
 	private Vector2 scroll;
 	private TextAsset stageCsv;
 	private Editor enemyEditor;
-	int selected;
+	private Event curEvent;
+	private List<Vector2> displayEnemyPos = new List<Vector2>();
+	private List<int> displayEnemyIndex = new List<int>();
+	private enum EditMode : int {
+		NOT_SELECT = 0,
+		SELECT_OBJ = 1,
+	}
+	EditMode editMode = EditMode.NOT_SELECT;
+
 	[MenuItem("Window/EnemySpawnEdit")]
 	static void Open(){
 		enemyDefine = Resources.Load ("ScriptableObjects/EnemyDefineData") as EnemyDefine;
@@ -22,11 +30,13 @@ public class EnemySpawnEditor : EditorWindow {
 	}
 
 	void OnEnable(){
-		targetEnemy = Resources.Load (enemyDefine.enemy [index].PATH) as GameObject;
+		targetEnemy = Resources.Load (enemyDefine.enemy [targetEnemyIndex].PATH) as GameObject;
+		enemyTextures = new Texture2D[enemyDefine.varietyNum];
 	}
 
 	void OnGUI () {
-		var curEvent = Event.current;
+		curEvent = Event.current;
+		wantsMouseMove = true;
 		stageCsv = EditorGUILayout.ObjectField ("EnemySpawnCsv", stageCsv, typeof(TextAsset), false) as TextAsset;
 		EditorGUILayout.BeginHorizontal ();
 
@@ -39,39 +49,62 @@ public class EnemySpawnEditor : EditorWindow {
 			sw.Flush();
 			sw.Close();
 		}
+		if (curEvent.type == EventType.MouseDown) {
+			if (curEvent.mousePosition.x <= 490) {
+				displayEnemyPos.Add (curEvent.mousePosition);
+				displayEnemyIndex.Add (targetEnemyIndex);
+				Repaint ();
+			}
+		}
+		for (int i = 0; i < displayEnemyPos.Count; i++) {
+			GUI.Box (new Rect (displayEnemyPos [i].x - 10, displayEnemyPos [i].y - 10, 20, 20), enemyTextures [displayEnemyIndex[i]]);
+		}
 		EditorGUILayout.EndVertical ();
 
 		EditorGUILayout.BeginVertical ();
 		scroll = EditorGUILayout.BeginScrollView(scroll);
-		if (enemyDefine != null) {
-			for (int i = 0; i < enemyDefine.varietyNum; i++) {
-				bool flag = GUILayout.Toggle (index == i, "ENEMY" + i.ToString (), "OL Elem");
-				if (flag != (index == i)) {
-					index = i;
-					targetEnemy = Resources.Load (enemyDefine.enemy [index].PATH) as GameObject;
-				}
-			}
-		}
 
-		if (curEvent.type == EventType.KeyDown && curEvent.keyCode == KeyCode.DownArrow) {
-			index = index < enemyDefine.varietyNum - 1 ? index + 1 : index;
-			targetEnemy = Resources.Load (enemyDefine.enemy [index].PATH) as GameObject;
-			Repaint ();
-		} else if (curEvent.type == EventType.KeyDown && curEvent.keyCode == KeyCode.UpArrow) {
-			index = index > 0 ? index - 1 : index;
-			targetEnemy = Resources.Load (enemyDefine.enemy [index].PATH) as GameObject;
-			Repaint ();
-		}
-
-		if (targetEnemy != null) {
-			Texture t = targetEnemy.GetComponent<SpriteRenderer> ().sprite.texture;
-			GUILayout.Box(t, GUILayout.Width(50), GUILayout.Height(50));
-		}
+		DisplayEnemyList ();
+		CheckKeyMoveAtEnemyList ();
+		DisplayTargetEnemy ();
 
 
 		EditorGUILayout.EndScrollView();
 		EditorGUILayout.EndVertical ();
-
 		EditorGUILayout.EndHorizontal ();
+	}
+
+	private void DisplayEnemyList(){
+		if (enemyDefine != null) {
+			for (int i = 0; i < enemyDefine.varietyNum; i++) {
+				bool flag = GUILayout.Toggle (targetEnemyIndex == i, "ENEMY" + i.ToString (), "OL Elem");
+				if (flag != (targetEnemyIndex == i)) {
+					targetEnemyIndex = i;
+					targetEnemy = Resources.Load (enemyDefine.enemy [targetEnemyIndex].PATH) as GameObject;
+				}
+			}
+		}
+	}
+
+	private void CheckKeyMoveAtEnemyList(){
+		if (curEvent.type == EventType.KeyDown && curEvent.keyCode == KeyCode.DownArrow) {
+			targetEnemyIndex = targetEnemyIndex < enemyDefine.varietyNum - 1 ? targetEnemyIndex + 1 : targetEnemyIndex;
+			targetEnemy = Resources.Load (enemyDefine.enemy [targetEnemyIndex].PATH) as GameObject;
+			Repaint ();
+		} else if (curEvent.type == EventType.KeyDown && curEvent.keyCode == KeyCode.UpArrow) {
+			targetEnemyIndex = targetEnemyIndex > 0 ? targetEnemyIndex - 1 : targetEnemyIndex;
+			targetEnemy = Resources.Load (enemyDefine.enemy [targetEnemyIndex].PATH) as GameObject;
+			Repaint ();
+		}
+	}
+
+	private void DisplayTargetEnemy(){
+		if (targetEnemy != null) {
+			if (enemyTextures [targetEnemyIndex] == null) {
+				Texture t = targetEnemy.GetComponent<SpriteRenderer> ().sprite.texture;
+				enemyTextures [targetEnemyIndex] = t;
+			} 
+			GUILayout.Box (enemyTextures [targetEnemyIndex], GUILayout.Width (30), GUILayout.Height (30));
+		}
 	}
 }
