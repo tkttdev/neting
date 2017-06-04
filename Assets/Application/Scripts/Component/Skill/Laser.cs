280da485541f873dac9935203921ab4db0425a30
yulon <yarboroughace@yahoo.co.jp>
2017-06-04 13:24:45 +0900

    レーザーの挙動変更

--- Assets/Application/Scripts/Component/Skill/Laser.cs
+++ Assets/Application/Scripts/Component/Skill/Laser.cs
@@ -29,11 +29,11 @@ public class Laser : MonoBehaviour {
 	public IEnumerator DestroyLaser() {
 		yield return new WaitForSeconds(0.5f);
 
-		iTween.ScaleTo(gameObject, iTween.Hash("x", 0.5, "time", 0.5f));
+		iTween.ScaleTo(gameObject, iTween.Hash("x", 0, "time", 0.5f));
 
 		yield return new WaitForSeconds(0.5f);
 
-		gameObject.transform.localScale = new Vector3(0.1f, 0, 0);
+		gameObject.transform.localScale = new Vector3(0.3f, 0, 0);
 		ObjectPool.I.Release(gameObject);
 	}
 
